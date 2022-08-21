33911f5a5fa837ff141a9b6d7b4fa4f028e9c2d9
devzana <lozanascbusiness@gmail.com>
2022-08-21 13:20:55 +0800

    Added upload post page to home for upload feature from elvis

--- components/Pages/Home/index.tsx
+++ components/Pages/Home/index.tsx
@@ -1,9 +1,83 @@
-import { Stack, Button } from "@chakra-ui/react"
+import { useForm } from 'react-hook-form'
+import {
+  Stack,
+  Heading,
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Textarea,
+  Button,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 const Home = () => {
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = useForm()
+
+  const onSubmit = async (values: any) => {
+      const request = await fetch(`http://localhost:3000/api/v1/post/new?userId=0d3fe7e6-3891-4a22-b4f5-c4e5c03bd69c`, 
+      { 
+        method: 'POST', 
+        body: JSON.stringify(values),
+        headers: new Headers({
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6eyJ1c2VySWQiOiIwZDNmZTdlNi0zODkxLTRhMjItYjRmNS1jNGU1YzAzYmQ2OWMiLCJ1c2VyTmFtZSI6ImpsaWdvbmVzIiwidXNlclR5cGUiOiJTVFVERU5UIn0sImlhdCI6MTY2MTAwMzU0OSwiZXhwIjoxNjYxMDA3MTQ5fQ.KC71rLKcVJHct08VVGLCI8eXUFEuDmrYdmCtZQVNqaw',
+          'Content-Type': 'application/json'
+        })
+      })
+      const result = await request.json()
+      console.log(result)
+  }
+
   return (
-    <Stack m="auto">
-      <Button>Coming Soon!</Button>
+    <Stack mt={4} w='full' px={4}>
+      {
+        isSubmitSuccessful && (
+        <Alert status='success'>
+          <AlertIcon />
+          Data uploaded to the server. Fire on!
+        </Alert>
+        )
+      }
+      <Heading>Upload a Post</Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl mt={3} isInvalid={Boolean(errors.name)}>
+          <FormLabel htmlFor='title'>Title</FormLabel>
+          <Input
+            id='title'
+            placeholder='Title'
+            {...register('title', {
+              required: 'This is required',
+            })}
+          />
+          <FormErrorMessage>
+            {Boolean(errors.title) && String(errors.title?.message)}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl mt={3} isInvalid={Boolean(errors.name)}>
+          <FormLabel htmlFor='content'>Content</FormLabel>
+          <Textarea
+            id='content'
+            placeholder='Whats on your mind...'
+            {...register('content', {
+              required: 'This is required',
+            })}
+          />
+          <FormErrorMessage>
+            {Boolean(errors.content) && String(errors.content?.message)}
+          </FormErrorMessage>
+        </FormControl>
+        <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+          Submit
+        </Button>
+      </form>
     </Stack>
   )
 }
